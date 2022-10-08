90f0d55c702a7558e77aa59203cb12a75d088ca7
Antipin-E-L <antipin.e.l@mail.ru>
2022-10-08 15:16:01 +0600

    edit notes

--- Project 0/game_v3_homework.py
+++ Project 0/game_v3_homework.py
@@ -1,8 +1,8 @@
 import numpy as np
-min_number = 1 # нижний предел чисел для загадывания
-# можно изменить с сохранностью работоспособности кода
-max_number = 100 # верхний предел чисел для загадывания
-# можно изменить с сохранностью работоспособности кода
+min_number = 1 # Нижний предел чисел для загадывания
+# Можно изменить с сохранностью работоспособности кода
+max_number = 100 # Верхний предел чисел для загадывания
+# Можно изменить с сохранностью работоспособности кода
 
 def random_predict(number:int=1) -> int:
     """Угадываем число используя бинарный поиск
@@ -16,15 +16,15 @@ def random_predict(number:int=1) -> int:
 
     count = 0
     global min_number, max_number 
-    # взять глобальные значения пределов для угадывания числа
+    # Взять глобальные значения пределов для угадывания числа
     loc_min_num, loc_max_num = min_number - 1, max_number 
-    # определить локальные пределы для угадывания конкретного числа
+    # Определить локальные пределы для угадывания конкретного числа
     while True:
         count += 1
         predict_num = round((loc_min_num + loc_max_num) / 2)
         # Определить среднее число между двумя пределами
         if number == predict_num:
-            break # выход из цикла, если угадали
+            break # Выход из цикла, если угадали
         elif number > predict_num:
             loc_min_num = predict_num
         elif number < predict_num:
@@ -36,23 +36,23 @@ def score_game(random_predict) -> int:
     угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        random_predict ([type]): Функция угадывания
 
     Returns:
-        int: среднее количество попыток
+        int: Среднее количество попыток
     """
     global min_number, max_number 
-    # взять глобальные пределы для загадыния числа
+    # Взять глобальные пределы для загадыния числа
 
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
+    count_ls = [] # Список для сохранения количества попыток
+    np.random.seed(1) # Фиксируем сид для воспроизводимости
     random_array = np.random.randint(min_number, max_number + 1, size=(1000)) 
-    # загадали список чисел
+    # Загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    score = int(np.mean(count_ls)) # Находим среднее количество попыток
 
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
